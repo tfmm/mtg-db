3d7dcf94e15108512fe27931029423f2ad774b60
Russ Long <rl@tfmm.co>
2015-04-04 19:32:52 -0400

    Building files and structure

---
+++ includes/menu.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+  </head>
+	<body>
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="library.php">Card Library</a></li>
+				<li><a href="decks.php">Current Decks</a></li>
+				<li><a href="forsale.php">Cards for Sale</a></li>
+      </ul>
+    </nav>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <title>TFMM's MTG Database</title>
+  </head>
+  <body>
+    <div class=header>
+      <h1>TFMM's MTG Database</h1>
+      <?php include("includes/menu.php");?>
+    </div>
+    <div class=content>
+      <p>See the links in the menu for more resources. I'll have my current library, cards for sale, and current decks listed eventually.</p>
+    </div>
+  </body>
+</html>  
